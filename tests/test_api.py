21743459af0e5b2426fbdceb64a5e775af0fced5
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2025-01-21 19:48:22 +0300

    add env, conftest, test

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,1 +1,23 @@
+from http import HTTPStatus
+from models.User import User
+import pytest
+import requests
 
+
+def test_users(app_url):
+    response = requests.get(f"{app_url}/api/users/")
+    assert response.status_code == HTTPStatus.OK
+
+    users = response.json()
+    for user in users:
+        User.model_validate(user)
+
+
+@pytest.mark.parametrize("user_id", [1, 6, 12])
+def test_user(app_url, user_id):
+    response = requests.get(f"{app_url}/api/users/{user_id}")
+    assert response.status_code == HTTPStatus.OK
+
+    user = response.json()
+    # validation response model
+    User.model_validate(user)
