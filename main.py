7a72bd067eaf54787469e27b2fc61df801fad332
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2025-01-21 19:14:27 +0300

    add structure of project

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
+import json
 import uvicorn
-from fastapi import FastAPI
 from http import HTTPStatus
+from fastapi import FastAPI
+from models.User import User
 
 app = FastAPI()
 
+# load file to memory
+users: list[User]
+
+
 @app.get("api/users/{user_id}", status_code=HTTPStatus.OK)
-def get_user(user_id):
-    pass
+def get_user(user_id) -> User:
+    return users[user_id - 1]
+
 
 @app.get("api/users/", status_code=HTTPStatus.OK)
-def get_users():
-    pass
+def get_users() -> list[User]:
+    return users
 
 
 if __name__ == "__main__":
+    # read file from memory
+    with open("users.json") as f:
+        users = json.load(f)
+    # validation dates in file
+    for user in users:
+        User.model_validate(user)
+    print("Users loaded")
+    # start server
     uvicorn.run(app, host="localhost", port=8002)
--- models/User.py
+++ models/User.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel, EmailStr, HttpUrl
 
 
-class User:
+class User(BaseModel):
     id: int
     email: EmailStr
     first_name: str
