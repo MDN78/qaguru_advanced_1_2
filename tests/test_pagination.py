ba27ef1558862bff89c4238b6635d3bb9a1b650c
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2025-01-22 16:12:39 +0300

    update project

--- tests/test_pagination.py
+++ tests/test_pagination.py
@@ -1,11 +1,10 @@
 import pytest
 import requests
-from http import HTTPStatus
+
 
 def test_page_and_size_in_users(app_url):
     response = requests.get(f"{app_url}/api/users/")
     data = response.json()
-    print(data)
     assert data["total"] == 12
     assert data["page"] == 1
     assert data["size"] == 50
@@ -15,7 +14,6 @@ def test_page_and_size_in_users(app_url):
 def test_page_size(app_url, page, size):
     response = requests.get(f"{app_url}/api/users/", params={"page": page, "size": size})
     res = response.json()
-    print(res)
     assert page == res['page']
     assert size == res['size']
 
