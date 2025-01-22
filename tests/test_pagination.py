bcb77c25dddc693cf87aa3c3935b8a6a0b1a14cf
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2025-01-22 20:03:31 +0300

    update project

--- tests/test_pagination.py
+++ tests/test_pagination.py
@@ -17,3 +17,13 @@ def test_page_size(app_url, page, size):
     assert page == data['page']
     assert size == data['size']
     assert len(data["items"]) == size
+
+
+def test_users_in_pages(app_url):
+    first_page = {"page": 2, "size": 4}
+    second_page = {"page": 3, "size": 4}
+    response_1 = requests.get(f"{app_url}/api/users/", params=first_page)
+    response_2 = requests.get(f"{app_url}/api/users/", params=second_page)
+    data_1 = response_1.json()
+    data_2 = response_2.json()
+    assert data_1['items'][1] != data_2['items'][1]
